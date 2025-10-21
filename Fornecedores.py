6151f0915a55e1d14564a4894755ab9c5cbbc77d
SandersonSB <sanderson9777@gmail.com>
2025-10-21 04:18:26 -0300

    Refactor final consolidation logic in Fornecedores.py

--- Fornecedores.py
+++ Fornecedores.py
@@ -487,6 +487,26 @@ else:
                     "Situação"
                 ] = df_detalhe["previsto"]
 
+            # =========================
+            # Consolidado final
+            # =========================
+            ##df_consolidado_final = pd.merge(df_consolidado, df_detalhe, on ="cpf", how="outer")
+            df_consolidado_final = df_consolidado.copy()
+            colunas_remover = [
+                "FALTA SEM JUSTIFICATIVA",
+                "ABONO DE HORAS",
+                "DECLARAÇÃO DE HORAS",
+                "AJUSTE DE HORAS",
+                "ATESTADO MÉDICO",
+                "FOLGA HABILITADA",
+                "SAÍDA ANTECIPADA",
+            ]
+            
+            # Só remove as colunas que existem no df
+            df_consolidado_final = df_consolidado.drop(
+                columns=[col for col in colunas_remover if col in df_consolidado.columns],
+                errors="ignore"
+            )
                 
             
             # =========================
@@ -510,26 +530,6 @@ else:
                 df_consolidado = pd.merge(df_consolidado, df_situacoes, on="cpf", how="outer")
     
 
-            # =========================
-            # Consolidado final
-            # =========================
-            ##df_consolidado_final = pd.merge(df_consolidado, df_detalhe, on ="cpf", how="outer")
-            df_consolidado_final = df_consolidado.copy()
-            colunas_remover = [
-                "FALTA SEM JUSTIFICATIVA",
-                "ABONO DE HORAS",
-                "DECLARAÇÃO DE HORAS",
-                "AJUSTE DE HORAS",
-                "ATESTADO MÉDICO",
-                "FOLGA HABILITADA",
-                "SAÍDA ANTECIPADA",
-            ]
-            
-            # Só remove as colunas que existem no df
-            df_consolidado_final = df_consolidado.drop(
-                columns=[col for col in colunas_remover if col in df_consolidado.columns],
-                errors="ignore"
-            )
 
 
 
